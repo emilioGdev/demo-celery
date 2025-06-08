f41b17c6e2fe0baca97de6c047c9434cde3f5eb6
Emílio Gabriel <emiliogabriel000@gmail.com>
2025-06-08 01:35:47 -0300

    adicionando modelo alert

--- demo_celery/demo/models.py
+++ demo_celery/demo/models.py
@@ -13,3 +13,18 @@ class Demo(models.Model):
 
     def __str__(self):
         return f"[{self.level}] {self.message[:30]}"
+    
+
+class Alert(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pendente'),
+        ('classified', 'Classificado'),
+    ]
+
+    message = models.TextField()
+    category = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return f"{self.message[:50]}... ({self.status})"
