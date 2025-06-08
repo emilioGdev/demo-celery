50426d4fc58d6af5ac98178348cc053060410a93
Emílio Gabriel <emiliogabriel000@gmail.com>
2025-06-08 01:44:20 -0300

    adicionando task celery para classificar alertas

--- demo_celery/demo/tasks.py
+++ demo_celery/demo/tasks.py
@@ -1,9 +1,27 @@
 from celery import shared_task
 import time
 
+from demo.classifier import classify_alert
+from demo.models import Alert
+
 @shared_task
 def process_demo(demo_id):
     print(f"🔔 Processando demo {demo_id}...")
     time.sleep(5)  # Simula um processamento demorado
     print(f"✅ demo {demo_id} processado!")
     return f"demo {demo_id} processado"
+
+@shared_task
+def classify_alert_task(alert_id):
+    try:
+        alert = Alert.objects.get(id=alert_id)
+        if alert.status == 'pending':
+            category = classify_alert(alert.message)
+            alert.category = category
+            alert.status = 'classified'
+            alert.save()
+            return f"Alert {alert_id} classified as {category}"
+        else:
+            return f"Alert {alert_id} already classified"
+    except Alert.DoesNotExist:
+        return f"Alert {alert_id} does not exist"
