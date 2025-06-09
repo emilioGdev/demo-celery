e8d4245cf4ffc09e53a5ac4ec23cebcf9e6e518a
Emílio Gabriel <emiliogabriel000@gmail.com>
2025-06-09 00:17:21 -0300

    criação de task para classificar alert

--- demo_celery/demo/tasks.py
+++ demo_celery/demo/tasks.py
@@ -1,25 +1,31 @@
 from celery import shared_task
-import time
-
-from demo.classifier import classify_alert
+import joblib
+import os
 from demo.models import Alert
+import time
+MODEL_PATH = os.path.join(os.path.dirname(__file__), 'alert_classifier.joblib')
+
+model = joblib.load(MODEL_PATH)
 
 @shared_task
 def process_demo(demo_id):
     print(f"🔔 Processando demo {demo_id}...")
-    time.sleep(5)  # Simula um processamento demorado
+    time.sleep(5)  
     print(f"✅ demo {demo_id} processado!")
     return f"demo {demo_id} processado"
-
 @shared_task
 def classify_alert_task(alert_id):
     try:
         alert = Alert.objects.get(id=alert_id)
         if alert.status == 'pending':
-            category = classify_alert(alert.message)
+            message = alert.message.lower()
+
+            category = model.predict([message])[0]
+
             alert.category = category
             alert.status = 'classified'
             alert.save()
+
             return f"Alert {alert_id} classified as {category}"
         else:
             return f"Alert {alert_id} already classified"
